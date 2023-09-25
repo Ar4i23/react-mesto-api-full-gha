0fdaa1ff79b2b87665f6464512cfa5ab80beb069
Arthur Antonov <artur-antonov-1992@mail.ru>
2023-09-25 11:55:00 +0300

    update url in app.js backend

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const bodyParser = require("body-parser");
+const { errors } = require("celebrate");
 
-const cors = require('cors');
-const errorHandler = require('./middlewares/error-handler');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require("cors");
+const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
-const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/cohort66' } =
-  process.env;
+const { PORT = 3000, DB_URL = "mongodb://127.0.0.1/cohort66" } = process.env;
 
 const app = express();
 const limiter = rateLimit({
@@ -32,7 +31,7 @@ mongoose.connect(DB_URL, {
 });
 app.use(requestLogger);
 app.use(limiter);
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 
 app.use(errorLogger);
 app.use(errors());
